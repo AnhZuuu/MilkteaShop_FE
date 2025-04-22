3bec3466f450e1f0a35c643dcf7c3308f0f98d3c
Tien <thuytienln238@gmail.com>
2025-04-22 16:24:39 +0700

    Fetch list of user + Filter by role and active + Search

---
+++ src/app/demoUI/page.tsx
@@ -0,0 +1,11 @@
+import UserTable from "@/components/usersManagement/UserTable"
+
+const adminPage = () => {
+ return (
+    <div className="space-y-6">
+          <UserTable />
+      </div>
+ )
+}
+
+export default adminPage;
