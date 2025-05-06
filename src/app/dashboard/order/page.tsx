809440264ccb639f39afa9e6b57238f97cf21541
Anh Vu <anhvu11102003@gmail.com>
2025-05-06 11:12:08 +0700

    add useEffect for mobile

--- src/app/dashboard/order/page.tsx
+++ src/app/dashboard/order/page.tsx
@@ -13,39 +13,44 @@ export default function Home() {
   const [user, setUser] = useState(null);
   const [width, setWidth] = useState(0);
 
-  
+  const [isMobile, setIsMobile] = useState(false);
+
   useEffect(() => {
     if (typeof window !== "undefined") {
-      setWidth(window.innerWidth);
+      setIsMobile(window.innerWidth < 768);
     }
   }, []);
 
 
+
   useEffect(() => {
-    const userData = localStorage.getItem("user");
-    if (!userData) {
-      router.replace("/"); 
-    } else {
-      setUser(JSON.parse(userData));
-      setLoading(false);
+    if (typeof window !== "undefined") {
+      const userData = localStorage.getItem("user");
+      if (!userData) {
+        router.replace("/");
+      } else {
+        setUser(JSON.parse(userData));
+        setLoading(false);
+      }
     }
+
   }, []);
 
   if (loading) return <div>Loading...</div>;
 
   return (
     <div className="p-6 sm:p-10 space-y-12 font-[family-name:var(--font-geist-sans)] bg-gray-100">
-    <h1 className="text-2xl font-bold text-center">Doanh thu</h1>
-    <OrderTable />
-    {/* Charts in one row */}
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-      <LineChartOne />
-      <BarChartOne />
-    </div>
+      <h1 className="text-2xl font-bold text-center">Doanh thu</h1>
+      <OrderTable />
+      {/* Charts in one row */}
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+        <LineChartOne />
+        <BarChartOne />
+      </div>
 
-    {/* OrderTable below charts */}
-    
-  </div>
+      {/* OrderTable below charts */}
+
+    </div>
     // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
     //   <h1>Doanh thu</h1>
     //   <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
