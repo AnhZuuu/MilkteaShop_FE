e3435437c5bd7b664e9af710904d9f1266980d21
Anh Vu <anhvu11102003@gmail.com>
2025-05-06 11:15:15 +0700

    fix app/bill

--- src/app/bill/page.tsx
+++ src/app/bill/page.tsx
@@ -3,16 +3,15 @@ import BillPage from "@/components/order/bill";
 import { useEffect, useState } from "react";
 
 
-const [width, setWidth] = useState(0);
-
-useEffect(() => {
-  if (typeof window !== "undefined") {
-    setWidth(window.innerWidth);
-  }
-}, []);
-
 
 export default function Bill() {
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setWidth(window.innerWidth);
+    }
+  }, []);
   return (
     <>
       <BillPage />
